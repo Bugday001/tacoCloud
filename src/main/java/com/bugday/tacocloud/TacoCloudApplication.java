e6d02128087f0c6de0fe663f030b10348d2a4668
Bugday001 <lyf0214white@gmail.com>
2023-04-19 13:35:22 +0000

    second

--- src/main/java/com/bugday/tacocloud/TacoCloudApplication.java
+++ src/main/java/com/bugday/tacocloud/TacoCloudApplication.java
@@ -3,6 +3,7 @@ package com.bugday.tacocloud;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+//pringBootApplication 表明这是一个应用，是一个组合注释
 @SpringBootApplication
 public class TacoCloudApplication {
 
